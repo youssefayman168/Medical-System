fa0517dee0b4ab961c80e38cbc5e20b8ccd32174
youssefayman168 <alphadevelopers6@gmail.com>
2024-05-25 16:20:43 +0300

    update user model

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
-    "users"
+    "users",
+    "activity",
+    "exports",
+    "patients",
+    "receipets",
 ]
 
 MIDDLEWARE = [
