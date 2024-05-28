765ef50f080bfc32602c40cfd22f7885c6213c84
youssefayman168 <alphadevelopers6@gmail.com>
2024-05-28 15:14:50 +0300

    changing database

--- demo/settings.py
+++ demo/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medicaldatabase',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
