5931f2f4ad22696bef665ba0de689ef5c421e061
youssefayman168 <alphadevelopers6@gmail.com>
2024-06-01 14:14:29 +0300

    add more endpoints

--- demo/settings.py
+++ demo/settings.py
@@ -192,7 +192,7 @@ CELERY_TIMEZONE = "Africa/Cairo"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "yosofaymanessawy@gmail.com"
-EMAIL_HOST_PASSWORD = "urlx idef uypy xvgs"
+EMAIL_HOST_PASSWORD = "csjf zhze zgap weoo"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
