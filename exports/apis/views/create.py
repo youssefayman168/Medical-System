6aa076c8ab1ed8d299d3ae9c272f05a787c594cb
youssefayman168 <alphadevelopers6@gmail.com>
2024-06-10 20:06:07 +0300

    upload new files

--- exports/apis/views/create.py
+++ exports/apis/views/create.py
@@ -64,7 +64,7 @@ def create_export(request):
             except Exception as e:
                 print(e)
                 return Response({
-                    "message": "حدث خطأ اثناء ادخال المنتجات الخاصة بالتوريد...حاول مرة اخري"
+                    "message": f"حدث خطأ اثناء ادخال المنتجات الخاصة بالتوريد...حاول مرة اخري {e}"
                 }, status=status.HTTP_400_BAD_REQUEST)
             
         try:
