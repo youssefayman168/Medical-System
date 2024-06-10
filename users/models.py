ab1d4720b330c35e09b7a4060c95ffe1085050b8
youssefayman168 <alphadevelopers6@gmail.com>
2024-06-10 21:54:35 +0300

    upload new files

--- users/models.py
+++ users/models.py
@@ -11,6 +11,7 @@ class User (AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
+
 class OTPCode(models.Model):
     otp = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
