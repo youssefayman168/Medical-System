e554b6cb3c5746ab8ce4dc644f7a0ee279db7d01
youssefayman168 <alphadevelopers6@gmail.com>
2024-06-11 23:51:36 +0300

    fix update

--- exports/apis/views/create.py
+++ exports/apis/views/create.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
-from rest_framework.decorators import permission_classes, api_view
+from rest_framework.decorators import permission_classes, api_view, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from exports.models import Export, Order
@@ -7,6 +8,7 @@ from activity.models import Activity
 
 @api_view(["POST"])
 @permission_classes([permissions.IsAdminUser])
+@parser_classes([FormParser, MultiPartParser])
 def create_export(request):
     data = request.data
     user = request.user 
