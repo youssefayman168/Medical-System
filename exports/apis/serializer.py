6a68adfa934e8d2423936000ec7833e39f84990c
youssefayman168 <alphadevelopers6@gmail.com>
2024-06-11 21:07:12 +0300

    fix update

--- exports/apis/serializer.py
+++ exports/apis/serializer.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from ..models import Export, Order
 
-class ExportSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Export
-        fields = "__all__"
-
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = "__all__"
+
+class ExportSerializer(serializers.ModelSerializer):
+    orders = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Export
+        fields = "__all__"
+
+    def get_orders(self, obj):
+        orders = Order.objects.filter(export=obj)
+        serializer = OrderSerializer(orders, many=True).data
+        return serializer
