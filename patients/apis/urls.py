a54461cf50eeeea5d2f9bdffb379a9c9794222ad
youssefayman168 <alphadevelopers6@gmail.com>
2024-06-23 13:02:13 +0300

    add analytics

--- patients/apis/urls.py
+++ patients/apis/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views.create import create_patient
 from .views.update import update_patient
 from .views.delete import delete_patient
+from .views.search import search_patients
 from .views.get import get_patient, get_all
 
 urlpatterns = [
@@ -9,5 +10,6 @@ urlpatterns = [
     path("update/<int:patient_id>/", update_patient),
     path("delete/<int:patient_id>/", delete_patient),
     path("get/<int:paitent_id>/", get_patient),
+    path("search/<int:doc_num>/", search_patients),
     path("get-all/", get_all),
 ]
