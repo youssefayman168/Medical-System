9ae11cc87a926fa5b24e2f79d81ae93450b6f689
youssefayman168 <alphadevelopers6@gmail.com>
2024-06-27 11:35:26 +0300

    add the rest of reports

--- reports/apis/urls.py
+++ reports/apis/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views.patients import get_patient_report, get_not_received_patients, get_received_patients
 
 urlpatterns = [
-    path("inclusive-patient/", get_patient_report),
+    path("inclusive-patient/<int:document_number>/", get_patient_report),
     path("received-patients/", get_received_patients),
     path("not-received-patients/", get_not_received_patients),
 ]
