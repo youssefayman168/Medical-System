f95ed148fdc3b4b995acb2f3a076e2d805f97677
youssefayman168 <alphadevelopers6@gmail.com>
2024-06-28 12:50:43 +0300

    change data to return paitent

--- receipets/apis/serializers.py
+++ receipets/apis/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from receipets.models import Receipet
-
+from patients.apis.serializers import PaitentSerializer
 class ReceipetSerializer(serializers.ModelSerializer):
+    patient = PaitentSerializer(read_only=True, many=False)
     class Meta:
         model = Receipet
         fields = "__all__"
