134871a466ad4fdecd0b52b6dbf5058d4895a125
youssefayman168 <alphadevelopers6@gmail.com>
2024-07-03 21:16:20 +0300

    add made_by

--- activity/apis/serializers.py
+++ activity/apis/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from ..models import Activity
+from users.apis.serializers import UserSerializer
 
 class ActivitySerializer(serializers.ModelSerializer):
+    made_by = UserSerializer(read_only=True, many=False)
     class Meta:
         model = Activity
         fields = "__all__"
