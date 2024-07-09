92c384d6574982edc3622766b4c31fcb58ed7ef0
youssefayman168 <alphadevelopers6@gmail.com>
2024-07-09 21:50:55 +0300

    try to fix error

--- exports/apis/serializer.py
+++ exports/apis/serializer.py
@@ -4,11 +4,15 @@ from ..models import Export, Order
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ("prod_name", "quantity")
+        fields = "__all__"
 
 class ExportSerializer(serializers.ModelSerializer):
-    orders = OrderSerializer(many=True)
+    orders = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Export
         fields = "__all__"
 
+    def get_orders(self, obj):
+        orders = Order.objects.filter(export=obj)
+        serializer = OrderSerializer(orders, many=True).data
+        return serializer
